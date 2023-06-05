3d7e518d4ca7efa57e426854f1efe307fcc97546
GITvoren <tejuco.voren@gmail.com>
2023-06-05 10:05:21 +0800

    added cors

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const cors = require('cors');
 port = process.env.PORT || 8080;
 
 // parsing middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+// cors
+app.use(cors({
+     origin: "*"
+    /*  methods: ["GET", "POST"] 
+     credentials: true */
+}))
 
 // main routes
 const userRoutes = require('./routes/userRoutes.js');
